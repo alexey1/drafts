940b179b81327eaf831c7eccbb50be9de39d1792
Алексей <63kotelnikov@gmail.com>
2026-09-12 12:12:30 +0400

    Update Рекурсия(Сумма четных чисел в списке).py

--- Рекурсия(Сумма четных чисел в списке).py
+++ Рекурсия(Сумма четных чисел в списке).py
@@ -4,4 +4,4 @@ def sum_even(lst):
     first = lst[0] if lst[0] % 2 == 0 else 0
     return first + sum_even(lst[1:])
 
-print(sum_even([1, 2, 3, 4, 5, 6]))  # 12
+print(sum_even([1, 2, 8, 9, 5, 6]))
